f3a725958399178c37e833385b3c80725a8b2603
Stephen Williams <stephen@wearetmbr.com>
2017-10-09 12:50:36 -0500

    datatypes updates

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// logs current date
+// Logs current date
 
 let displayDate = function() {
 	let currentDate = new Date();
@@ -9,95 +9,76 @@ let displayDate = function() {
 }
 displayDate();
 
-// convert number to string
+// Convert number to string
 
-let convertNumber = function() {
-	let newNumber = 5;
-	toString(newNumber);
-	console.log(newNumber);
-	console.log(typeof(newNumber));
+let convertNumber = function(x) {
+	let newString = x.toString();
+	console.log('The string is: ' + newString);
+	console.log('The string is a type of: ' + typeof(newString));
 }
-
-convertNumber();
+convertNumber(15);
 
 
 // Convert string to number
 
 let convertString = function(x) {
-	let newString = parseInt(x);
-	console.log(x);
-	console.log(typeof(x));
+	let newNumber = parseInt(x);
+	console.log('The number is: ' + newNumber);
+	console.log('The string is a type of: ' + typeof(newNumber));
 }
-
 convertString('6');
 
 
-// display datatype of variable
+// Display datatype of variable
 
 let dataTypes = function(y) {
-	let type = typeof(y);
-	console.log(type);
+	let newType = typeof(y);
+	console.log('This is type of: ' + newType);
 }
-
 dataTypes('true');
 
-// adding two integers
+// Adding two integers
 
 let calculator = function(a, b){
 	let sum = a + b;
-	console.log(sum);
-	console.log(typeof(sum));
+	console.log('This sum of the two numbers is: ' + sum);
+	console.log('The sum is type of: ' + typeof(sum));
 }
 calculator(2, 6);
 
-// run if both things are true
-
+// Run if both things are true
 
 let twoTruths = function(c,d){
-
 	if ( c > 0 && d > 0 ) {
-		console.log('both true');
+		console.log('Both expressions are true');
 	} else {
-		console.log('incorrect');
+		console.log('One expression is false');
 	}
 }
-
 twoTruths(3, 6);
 
 
-// run if one of two things are true
+// Run if one of two things are true
 
 let oneTruth = function(e,f){
-
 	if ( e > 0 || f > 0 ) {
-		console.log('one is true');
+		console.log('One expression is true');
 	} else {
-		console.log('incorrect');
+		console.log('Neither expression is true');
 	}
 }
+oneTruth(-3, 6);
 
-oneTruth(3, -6);
 
+// Run if both things are false
 
-// run if both things are false
-
-let twoFalse = function(c,d){
-
-	if ( c < 0 && d < 0 ) {
-		console.log('incorrect');
+let twoFalse = function(g,h){
+	if ( g < 0 && h < 0 ) {
+		console.log('One expression is true');
+	} else if ( g < 0 || h < 0 ) {
+		console.log('One expression is true');
 	} else {
-		console.log('both false');
+		console.log('Both expressions are False');
 	}
 }
-
 twoFalse(3, 6);
-
-// Hal's example
-
-let strConvert = function(testString){
-	console.log('the variable is set as' + (typeof(testString)) + ' ' + testString);
-	let newInt = Number(testString);
-	console.log('now it is the ' + (typeof(newInt)) + ' ' +newInt)
-}
-
-strConvert('10');
