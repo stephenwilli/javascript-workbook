5c71822edaeb6ff079edb3a172d1b71d725e93e5
Stephen Williams <stephen@wearetmbr.com>
2017-12-04 20:52:07 -0600

    fetching users

--- 08week/fetch/script.js
+++ 08week/fetch/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+class ListUsers extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    }
+  }
+  
+  componentWillMount(){
+    const fetchedUsers = [];
+      fetch('http://reqres-api.herokuapp.com/api/users?page=2').then((result) => {
+        return result.json();
+      }).then((response) => {
+        
+        fetchedUsers.push(response);
+        console.log(fetchedUsers);
+          this.setState({
+            users: fetchedUsers
+          })
+        });
+    }
+
+  render() {
+    
+    return(
+      <div className="">
+        <ul>
+        {this.state.users.map((name) => {
+            return <li>{name.first_name}</li>
+          })
+        }
+        </ul>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<ListUsers />, document.getElementById('fetch'));
